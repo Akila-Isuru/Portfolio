8064566e1034d439a51828545d011184af7e293d
akilaisuru <akilaisurukahingala@gmail.com>
2025-11-11 12:05:00 +0530

    change the bio

--- CSS/script.js
+++ CSS/script.js
@@ -4,6 +4,7 @@ var typed = new Typed("#auto-type", {
     "Mobile Developer",
     "Web Developer",
     "Fullstack Developer",
+    "AI % ML Engineer",
   ],
   typeSpeed: 70, // Increased from 50
   backSpeed: 40, // Decreased from 50
